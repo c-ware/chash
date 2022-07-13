8b22bf2f9414dbafdf37f6815716b0ec2fa3f693
adm@tsbsd4.hsnet.local <hackersmackertech@gmail.com>
2022-07-13 15:33:36 -0500

    feat: performance tests

---
+++ tests/extreme_test2.c
@@ -0,0 +1,88 @@
+/*
+ * An extreme test of chash that is immune to CPU caching from successive runs.
+*/
+
+/*
+#define CHASH_RESIZE(size) \
+   ((size) * 2.0)
+*/
+
+/* #define CHASH_LOAD_THRESHOLD 0.5 */
+
+#include "common.h"
+#include <time.h>
+
+/* Number of iterations */
+#define LIMIT 600000
+
+/* Since we're going for the fastest possible speed, globals are used. */
+struct StringHashtable* stress_table;
+int i = 0;
+int value = -1;
+
+/* Test data */
+char strings[LIMIT][16];
+
+/* Valid characters for random strings */
+char charset[] = "0123456789"
+    "abcdefghijklmnopqrstuvwxyz"
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+/* Make a random string */
+void rand_str(char* dest, size_t length) {
+    while(length-- > 0) {
+        size_t pos = (double) rand() / RAND_MAX * (sizeof charset - 1);
+        *dest++ = charset[pos];
+    }
+    *dest = '\0';
+}
+
+
+int main(void) {
+    clock_t start, halfway, end;
+    
+    /* Generate the test data. */
+    printf("Generating strings...\n");
+
+    for(i = 0; i < LIMIT; i++) {
+        rand_str(strings[i], 16);
+    }
+
+    printf("Execution begins...\n");
+
+
+    /* Start time marker */
+    start = clock();
+    
+    /* Generate the hashtable */
+    stress_table = chash_init(stress_table, STRING_TABLE);
+
+    /* Add each string */
+    for(i = 0; i < LIMIT; i++) {
+        chash_assign(stress_table, strings[i], i, STRING_TABLE);
+    }
+
+    /* Done with fill time marker */
+    halfway = clock();
+    
+    /* Correct length? */
+    printf("Length is now %d\n", stress_table->length);
+    assert(stress_table->length == LIMIT);
+
+    /* Verify each string is inside the hashtable, and they have the
+     * correct value */
+    for(i = 0; i < LIMIT; i++) {
+        value = chash_lookup(stress_table, strings[i], value, STRING_TABLE);
+        assert(value == i);
+    }
+
+    chash_free(stress_table, STRING_TABLE);
+
+    /* Measure and calculate the runtime */
+    end = clock();
+    printf("Fill time: %f\n", ((double)(halfway - start) / CLOCKS_PER_SEC));
+    printf("Read time: %f\n", ((double)(end - halfway) / CLOCKS_PER_SEC));
+    printf("Total time: %f\n", ((double)(end - start) / CLOCKS_PER_SEC));
+
+    return EXIT_SUCCESS;
+}
